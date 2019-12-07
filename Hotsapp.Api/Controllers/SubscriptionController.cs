63f434179c7a7a0c86425117d204644ccad60341
Fabio <fabiolux999@gmail.com>
2019-12-07 01:21:57 -0300

    Create subscription controller

---
+++ Hotsapp.Api/Controllers/SubscriptionController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotsapp.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SubscriptionController : BaseController
+    {
+        public async Task<IActionResult> GetStatus()
+        {
+            return Ok();
+        }
+
+        public async Task<IActionResult> CreateSubscription()
+        {
+            return Ok();
+        }
+
+        public async Task<IActionResult> CancelSubscription()
+        {
+            return Ok();
+        }
+    }
+}

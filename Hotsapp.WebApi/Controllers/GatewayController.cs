97b4817c2a4e364b14b178c5ecafb4f94310f7fe
Fabio <fabiolux999@gmail.com>
2020-06-17 23:37:35 -0300

    Add Streamer Gateway

---
+++ Hotsapp.WebApi/Controllers/GatewayController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotsapp.WebApi.Controllers
+{
+    [Route("api/gateway")]
+    [ApiController]
+    public class GatewayController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index([FromQuery] Guid channelId)
+        {
+            Response.Cookies.Append("routeKey", channelId.ToString());
+            return Ok(new { url = "https://api.hotsapp.net/streamer/streamhub?channelId=9fc680b0-a6cd-11ea-87a6-02dd375f4dba" });
+        }
+    }
+}

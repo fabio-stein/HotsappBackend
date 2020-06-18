c187a0d295f1c9e6c69e5f43a763718ed5f277a8
Fabio <fabiolux999@gmail.com>
2020-06-18 19:38:48 -0300

    Add ChannelInfo controller

---
+++ Hotsapp.WebApi/Controllers/ChannelInfoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hotsapp.Data.Util;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotsapp.WebApi.Controllers
+{
+    [Route("api/channel-info")]
+    [ApiController]
+    [AllowAnonymous]
+    public class ChannelInfoController : ControllerBase
+    {
+        [HttpGet("status")]
+        public async Task<IActionResult> GetChannelInfo([FromQuery] Guid channelId)
+        {
+            using (var ctx = DataFactory.GetDataContext())
+            {
+                var channel = await ctx.Channel.FirstOrDefaultAsync(c => c.Id == channelId);
+
+                if (channel == null)
+                    return NotFound();
+
+                return Ok(new
+                {
+                    title = channel.Title,
+                });
+            }
+        }
+    }
+}
--- Hotsapp.WebApi/Controllers/GatewayController.cs
+++ Hotsapp.WebApi/Controllers/GatewayController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +10,7 @@ namespace Hotsapp.WebApi.Controllers
 {
     [Route("api/gateway")]
     [ApiController]
+    [AllowAnonymous]
     public class GatewayController : ControllerBase
     {
         [HttpGet]

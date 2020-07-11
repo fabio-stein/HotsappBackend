dc0de9cbad55602022394680f912b2cbb60e0a3b
Fabio <fabiolux999@gmail.com>
2020-07-11 19:54:30 -0300

    PlaylistWorker - Start/Stop Channel by messaging

--- Hotsapp.PlaylistWorker/Worker/WorkerManager.cs
+++ Hotsapp.PlaylistWorker/Worker/WorkerManager.cs
@@ -18,9 +18,9 @@ namespace Hotsapp.PlaylistWorker
         private readonly PlaylistRepository _playlistService;
         private readonly ChannelWorkerFactory _channelWorkerFactory;
         private CancellationToken _ct;
-        private List<ChannelWorker> workerList = new List<ChannelWorker>();
 
-        private List<Guid> channels;
+        private List<ChannelWorker> workerList = new List<ChannelWorker>();
+        private object _workerListLock = new object();
 
         public WorkerManager(PlaylistWorkerMessagingService messagingService, PlaylistRepository playlistService, ChannelWorkerFactory channelWorkerFactory)
         {
@@ -32,17 +32,18 @@ namespace Hotsapp.PlaylistWorker
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             _ct = stoppingToken;
+
             await ResumeRunningChannels();
+            RegisterPlaylistMessagingController();
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                /*                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                                await _messagingService.PublishForTag(DateTime.UtcNow.ToString(), "dev");
-                                await _playlistService.PlayNext(new Guid("1e602dbd-a6cc-11ea-87a6-02dd375f4dba"));*/
                 try
                 {
-                    await Task.Delay(5000, stoppingToken);
+                    await Task.Delay(10000, stoppingToken);
+                    _log.Information("WorkerManager Up");
                 }
-                catch (Exception e)
+                catch (Exception)
                 {
                     _log.Information("Worker delay cancelled");
                 }
@@ -57,30 +58,112 @@ namespace Hotsapp.PlaylistWorker
         private async Task ResumeRunningChannels()
         {
             _log.Information("Resuming already running channels");
+
+            List<Guid> channelsToResume;
             using (var ctx = DataFactory.GetDataContext())
             {
-                channels = await ctx.Channel.Where(c => c.Status == "RUNNING").Select(c => c.Id).ToListAsync();
+                channelsToResume = await ctx.Channel.Where(c => c.Status == "RUNNING").Select(c => c.Id).ToListAsync();
             }
-            var runningMedia = await _playlistService.LoadCurrentChannelMedia(channels);
 
-            _log.Information("Resuming {0} channels with already running media", runningMedia.Count());
-            foreach (var channel in runningMedia)
+            await StartChannels(channelsToResume);
+            _log.Information("Finished resuming channels");
+        }
+
+        private async Task StartChannels(List<Guid> channelIds)
+        {
+            foreach(var channel in channelIds.ToList())
             {
-                var worker = _channelWorkerFactory.CreateWorker();
-                workerList.Add(worker);
-                worker.LoadStatus(channel);
-                worker.Start(_ct, channel.ChannelId);
+                lock (_workerListLock)
+                {
+                    if (workerList.Any(w => w.ChannelId == channel))
+                    {
+                        _log.Information("[{0}] Skipping channel already in list");
+                        channelIds.Remove(channel);
+                    }
+                }
             }
 
-            var pendingChannels = channels.Where(c => !runningMedia.Any(m => m.ChannelId == c)).ToList();
+            var runningMedia = (await _playlistService.LoadCurrentChannelMedia(channelIds)).ToList();
 
-            _log.Information("Starting {0} channels with new media", pendingChannels.Count);
-            foreach (var channel in pendingChannels)
+            if (runningMedia.Count > 0)
             {
-                var worker = _channelWorkerFactory.CreateWorker();
-                workerList.Add(worker);
-                worker.Start(_ct, channel);
+                _log.Information("Resuming {0} channels with already running media", runningMedia.Count());
+                foreach (var channel in runningMedia)
+                {
+                    var worker = _channelWorkerFactory.CreateWorker();
+                    lock (_workerListLock)
+                    {
+                        workerList.Add(worker);
+                    }
+                    worker.LoadStatus(channel);
+                    worker.Start(_ct, channel.ChannelId);
+                }
             }
+
+            var channelsToInitialize = channelIds.Where(c => !runningMedia.Any(m => m.ChannelId == c)).ToList();
+
+            if (channelsToInitialize.Count > 0)
+            {
+                _log.Information("Starting {0} channels with new media", channelsToInitialize.Count);
+                foreach (var channel in channelsToInitialize)
+                {
+                    var worker = _channelWorkerFactory.CreateWorker();
+                    lock (_workerListLock)
+                    {
+                        workerList.Add(worker);
+                    }
+                    worker.Start(_ct, channel);
+                }
+            }
+        }
+
+        private async Task StopChannels(List<Guid> channelIds)
+        {
+            var workers = workerList.Where(w => channelIds.Contains(w.ChannelId)).ToList();
+            foreach (var worker in workers)
+            {
+                try
+                {
+                    _ = Task.Run(async () =>
+                      {
+                          try
+                          {
+                              await worker.Stop();
+                          }
+                          catch (Exception e)
+                          {
+                              _log.Error(e, "[{0}] Failed to stop channel", worker.ChannelId);
+                          }
+
+                          lock (_workerListLock)
+                          {
+                              workerList.Remove(worker);
+                          }
+                          _log.Information("[{0}] Channel stopped", worker.ChannelId);
+                      });
+                }
+                catch (Exception e)
+                {
+                    _log.Error(e, "[{0}] Failed to remove channel", worker.ChannelId);
+                }
+            }
+        }
+
+        private void RegisterPlaylistMessagingController()
+        {
+            _messagingService.OnChannelControllerMessage += (async (obj, message) =>
+            {
+                if (message.Action == PlaylistWorkerMessagingService.ChannelControllerMessageAction.START)
+                {
+                    _log.Information("[{0}] Received message to start channel", message.ChannelId);
+                    await StartChannels(new List<Guid>() { message.ChannelId });
+                }
+                else if (message.Action == PlaylistWorkerMessagingService.ChannelControllerMessageAction.STOP)
+                {
+                    _log.Information("[{0}] Received message to stop channel", message.ChannelId);
+                    await StopChannels(new List<Guid>() { message.ChannelId });
+                }
+            });
         }
     }
 

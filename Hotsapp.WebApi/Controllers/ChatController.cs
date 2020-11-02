1527e100ac51ed031352e2432fe734d6bc4fcc03
Fabio <fabiolux999@gmail.com>
2020-11-02 04:27:38 -0300

    update

--- Hotsapp.WebApi/Controllers/ChatController.cs
+++ Hotsapp.WebApi/Controllers/ChatController.cs
@@ -20,8 +20,9 @@ namespace Hotsapp.WebApi.Controllers
             var userId = User.GetUserId();
             using (var conn = DataFactory.OpenConnection())
             {
-                var res = await conn.QueryAsync(@"SELECT c.* FROM wa_phone p
+                var res = await conn.QueryAsync(@"SELECT c.*, a.Title AS AreaTitle FROM wa_phone p
 INNER JOIN wa_chat c ON c.PhoneNumber = p.Number
+INNER JOIN wa_phone_area a ON a.PhoneNumber = p.Number AND a.Id = c.Area
 WHERE p.OwnerId = @userId
 AND c.IsActive", new { userId });
                 return Ok(res);
@@ -61,6 +62,27 @@ AND c.IsActive", new { userId, data.ChatId });
                 var res = await conn.QueryAsync(@"
 INSERT INTO wa_chat_message (ChatId, ChatPhoneNumber, Body, IsFromMe, IsProcessed) VALUES (@chatId, @phoneNumber, @body, TRUE, FALSE);
 ", new { chatId = data.ChatId, phoneNumber = chat.PhoneNumber, data.Body });
+                return Ok();
+            }
+        }
+
+        [HttpPost("{chatId}")]
+        public async Task<IActionResult> CloseChat(int chatId)
+        {
+            var userId = User.GetUserId();
+            using (var conn = DataFactory.OpenConnection())
+            {
+                var chat = await conn.QueryFirstOrDefaultAsync<WaChat>(@"SELECT c.* FROM wa_chat c
+INNER JOIN wa_phone p ON p.Number = c.PhoneNumber
+WHERE p.OwnerId = @userId
+AND c.IsActive", new { userId, chatId });
+
+                if (chat == null)
+                    return BadRequest("Invalid chat");
+
+                var res = await conn.QueryAsync(@"
+UPDATE wa_chat SET IsActive = FALSE WHERE Id = @chatId
+", new { chatId });
                 return Ok();
             }
         }
